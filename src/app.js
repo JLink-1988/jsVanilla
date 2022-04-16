ff6e79c0aedfda1a120329ebd848d5fe4e389c57
JLink-1988 <yasmanyjaneirogonzalez@gmail.com>
2022-04-16 03:23:57 +0000

    The Excuse Generator

--- src/app.js
+++ src/app.js
@@ -5,7 +5,34 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
+function getRandomInt(max) {
+  return Math.floor(Math.random() * max);
+}
+
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  const myString = "OMG! you will not believe me but...";
+
+  let who = ["The dog", "My Door", "Her cat", "My lego"];
+  let action = ["broke", "peed", "crash", "hit"];
+  let what = ["the car", "my laptop", "my bicicle"];
+  let when = [
+    "yesterday",
+    "this morning",
+    "this evening",
+    "last week",
+    "while i was lounch"
+  ];
+  document.body.innerHTML =
+    "<h1>" +
+    myString +
+    "</h1>" +
+    "<h1>" +
+    who[getRandomInt(4)] +
+    " " +
+    action[getRandomInt(4)] +
+    " " +
+    what[getRandomInt(3)] +
+    " " +
+    when[getRandomInt(5)] +
+    "</h1>";
 };
